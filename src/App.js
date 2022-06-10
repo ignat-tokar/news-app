a566e8ee007ad428d56f15073b1a36ec0050efb7
ateros <ateros@example.com>
2022-06-10 14:19:07 +0300

    adding useState and useEffect functionality (#5)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
 
-const src = "https://content.guardianapis.com/search?page=2&q=debate&api-key=test";
+// const src = "https://content.guardianapis.com/search?page=2&q=debate&api-key=test";
+// axios.get(src).then(data => {
+//   console.log(data);
+// });
 
-function App() {  
+function App() {
 
-  axios.get(src).then(data => {
-    console.log(data);
-  });
-  
+  const [result, setResult] = useState(0);
+  const [result2, setResult2] = useState(0);
+
+  useEffect(()=>{
+    setResult2(result*2);
+  }, [result]);
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Result = {result}</h1>
+      <h1>Result * 2 = {result2}</h1>
+      <button 
+        onClick={()=>{
+          setResult(result+1);
+        }}
+      >Plus Result</button>
     </div>
   );
 }
