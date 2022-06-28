8f5d214ae5cd25a856b2a61d0f453e374badc57c
Ignat Tokar <example@example.com>
2022-06-28 15:53:25 +0300

    added react-router-dom #10

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import { getArticlesByWord } from './api';
 import './App.css';
+import Media from './components/Media';
+import Politics from './components/Politics';
+import Sport from './components/Sport';
 
 function App() {
 
   const [articles, setArticles] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
     getData('debates');
-  },[]);
+  }, []);
 
   const getData = async (word) => {
     const data = await getArticlesByWord(word);
     setArticles(data.data.response.results);
   }
 
+  const inputHandler = (e) => {
+    setSearchWord(e.target.value);
+  }
+
+  const buttonHandler = (e) => {
+    getData(searchWord);
+  }
+
   return (
-    <div className="App">
-      {articles.map(article => {
-        return (
-          <p key={article.id}>
-            <a href={article.webUrl}>{article.webTitle}</a>
-          </p>
-        );
-      })}
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <input
+          value={searchWord}
+          onChange={inputHandler}
+        />
+        <button onClick={buttonHandler}>Search</button>
+        <div>
+          <NavLink to="/politics">Politics</NavLink>
+          <NavLink to="/sport">Sport</NavLink>
+          <NavLink to="/media">Media</NavLink>
+        </div>
+        <Routes>
+          <Route path="/politics" element={<Politics />} />
+          <Route path="/sport" element={<Sport />} />
+          <Route path="/media" element={<Media />} />
+        </Routes>
+        {articles.map(article => {
+          return (
+            <p key={article.id}>
+              <a href={article.webUrl}>
+                {article.webTitle.split(searchWord)[0]}
+                <span style={{ backgroundColor: 'yellow' }}>{searchWord}</span>
+                {article.webTitle.split(searchWord)[1]}
+              </a>
+            </p>
+          );
+        })}
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Politics.js
@@ -0,0 +1,7 @@
+function Politics () {
+  return (
+    <h1>Politics</h1>
+  );
+}
+
+export default Politics;
